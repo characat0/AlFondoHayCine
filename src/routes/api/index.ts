606e73fac4610ccfe8f129a0011f7afd926d4fee
MarcoVela <mvelar@uni.pe>
2020-04-10 13:33:39 -0500

    FFMPEG custom command

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,8 +2,8 @@ import {Application, Router} from "express";
 import SocketIO = require("socket.io");
 import { APIKEY } from "../../config";
 import { Mp4Segmenter } from "../../lib/Mp4Segmenter";
-import {segmenter, videoInfo} from "../../constants";
-import * as FFmpeg from "fluent-ffmpeg";
+import {segmenter, videoInfo, viewers} from "../../constants";
+import { spawn } from "child_process";
 
 export const apiRoute = Router();
 
@@ -67,6 +67,32 @@ apiRoute.post('/', (req, res) => {
     })
 });
 
+apiRoute.post('/cheat', (req, res) => {
+    const { key } = req.query;
+    const app : Application = req.app;
+    if (!key || key !== APIKEY) {
+        res.sendStatus(403);
+        return ;
+    }
+    res.sendStatus(200);
+    let { number, time, interval } = req.body;
+    console.log(req.body);
+    number = parseInt(number);
+    time = parseInt(time);
+    interval = parseInt(interval);
+    console.log(number, time, interval);
+    for (let i = 0; i < number; i++) {
+        setTimeout(() => {
+            app.set(viewers, app.get(viewers) + 1);
+            console.log(app.get(viewers));
+            setTimeout(() => {
+                app.set(viewers, app.get(viewers) - 1);
+                console.log(app.get(viewers));
+            }, (time * 1000) + Math.floor(Math.random() * interval) * 1000)
+        }, Math.floor(Math.random() * interval * 1000 ))
+    }
+});
+
 apiRoute.post('/fs', (req, res) => {
     const { key } = req.query;
     const app : Application = req.app;
@@ -95,15 +121,38 @@ apiRoute.post('/fs', (req, res) => {
     mp4Segmenter.on('data', (data:Buffer) => {
         console.log('data', data.length);
     });
-    const command = FFmpeg({ source: ruta });
+    const ffmpeg = spawn('ffmpeg', [
+        '-re',
+        '-i', ruta,
+        //'-g', '230',
+        '-preset', 'ultrafast',
+        '-vcodec', 'copy',
+        '-tune', 'zerolatency',
+        '-bufsize', '500M',
+        //'-b:v', '1.2M',
+        '-movflags', '+frag_keyframe+empty_moov+default_base_moof',
+        '-f', 'mp4',
+        'pipe:1'
+    ]);
+    ffmpeg.stdout.pipe(mp4Segmenter);
+    ffmpeg.stderr.setEncoding('utf8');
+    ffmpeg.stderr.on('data', function(data) {
+        console.log(data);
+        if(/^execvp\(\)/.test(data)) {
+            console.log('failed to start ffmpeg');
+            process.exit(1);
+        }
+    });
+    /*const command = FFmpeg({ source: ruta });
     command
         .withInputOption('-loglevel', 'debug')
         .withInputOptions(['-re'])
         .fromFormat('mp4')
+        .withOutputOption('-g', ' 52')
         .withVideoCodec('copy')
         .toFormat('mp4')
         .addOptions('-movflags', '+frag_keyframe+empty_moov+default_base_moof')
-        .writeToStream(mp4Segmenter);
+        .writeToStream(mp4Segmenter);*/
 /*    const stream = command.pipe();
     stream.on('data', chunk => {
         console.log(chunk);
@@ -112,7 +161,7 @@ apiRoute.post('/fs', (req, res) => {
         console.log("ffmpeg stdout:\n" + stdout);
         console.log("ffmpeg stderr:\n" + stderr);
     });*/
-    command.on('error', console.error);
+    //command.on('error', console.error);
     mp4Segmenter.on('error', (e) => {
         mp4Segmenter.removeAllListeners('data');
         app.set(segmenter, new Mp4Segmenter());
